eb1e9d0ebe2680446cb839bf6421f1a66d694d94
Patrick Reid <patrickjreid@gmail.com>
2022-08-24 20:31:27 -0600

    added styling to landing page Co-authored-by: Mariah 76408932+MariahTali@users.noreply.github.com Co-authored-by: Patrick 92654334+flyingwolf1701@users.noreply.github.com Co-authored-by: Yuki 93238813+nukiyaka@users.noreply.github.com

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,13 +13,18 @@ export const LandingPage: LandingPageComponent = () => {
     <Box style={{display: 'flex', justifyContent: 'space-around'  }} >
       
 
-      <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', p: 1, m: 1, }}>
+      <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'space-around', p: 1, m: 1, }}>
         <TextField sx={{m:1}} label='UserName' variant='outlined' defaultValue={'Type Username Here'}></TextField>
         <Button sx={{m:1}} variant="contained" size="large">Start A Session</Button>
         <Button sx={{m:1}}variant="contained" size="large">Join Session</Button>
       </Box>
-      <Box>
-      <Card variant="outlined">This is an MUI Card to put sessions</Card>
+      <Box sx={{m:1}}>
+      <Card variant="outlined">Sessions will live here, render different component?
+        <p>Join session 1</p>
+        <p>Join session 2</p>
+        <p>Join session 3</p>
+        
+      </Card>
       </Box>
 
     </Box>
