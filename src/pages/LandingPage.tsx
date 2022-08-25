aa7e16aa3c34e7681cefe8e38e5b19464e9e4018
Patrick Reid <patrickjreid@gmail.com>
2022-08-24 20:36:33 -0600

    added styling to landing page Co-authored-by: Mariah 76408932+MariahTali@users.noreply.github.com Co-authored-by: Patrick 92654334+flyingwolf1701@users.noreply.github.com Co-authored-by: Yuki 93238813+nukiyaka@users.noreply.github.com

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,18 +8,19 @@ type LandingPageComponent = (props: LandingPageProps) => JSX.Element;
 
 export const LandingPage: LandingPageComponent = () => {
   return (
-    <div style={{maxWidth: '100%'}}>
+    <div style={{maxHeight:'100%', maxWidth: '100%'}}>
 
-    <Box style={{display: 'flex', justifyContent: 'space-around'  }} >
+    <Box style={{display: 'flex', justifyContent: 'space-around', }} >
       
 
-      <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'space-around', p: 1, m: 1, }}>
+      <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'space-around', p: 1, m: 1, maxHeight:'100%' }}>
         <TextField sx={{m:1}} label='UserName' variant='outlined' defaultValue={'Type Username Here'}></TextField>
         <Button sx={{m:1}} variant="contained" size="large">Start A Session</Button>
         <Button sx={{m:1}}variant="contained" size="large">Join Session</Button>
       </Box>
       <Box sx={{m:1}}>
       <Card variant="outlined">Sessions will live here, render different component?
+      <h3>Open Sessions</h3>
         <p>Join session 1</p>
         <p>Join session 2</p>
         <p>Join session 3</p>
