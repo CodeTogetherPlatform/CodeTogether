8adbddb3badabd9c08accc8a9765ab42b7ee1402
Patrick Reid <patrickjreid@gmail.com>
2022-08-24 20:13:07 -0600

    building MUI on LandingPage
    Co-authored-by: Mariah 76408932+MariahTali@users.noreply.github.com
    Co-authored-by: Patrick 92654334+flyingwolf1701@users.noreply.github.com
    Co-authored-by: Yuki 93238813+nukiyaka@users.noreply.github.com

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import{ Box, TextField, Card } from '@mui/material';
 
 interface LandingPageProps { };
 
@@ -7,6 +8,22 @@ type LandingPageComponent = (props: LandingPageProps) => JSX.Element;
 
 export const LandingPage: LandingPageComponent = () => {
   return (
-    <Button variant="contained" size="large">Username Here</Button>
+    <div style={{maxWidth: '100%'}}>
+
+    <Box style={{display: 'flex', justifyContent: 'space-around'  }} >
+      
+
+      <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', p: 1, m: 1, }}>
+        <TextField sx={{m:1}} label='UserName' variant='outlined' defaultValue={'Type Username Here'}></TextField>
+        <Button sx={{m:1}} variant="contained" size="large">Start A Session</Button>
+        <Button sx={{m:1}}variant="contained" size="large">Join Session</Button>
+      </Box>
+      <Box>
+      <Card variant="outlined">This is an MUI Card to put sessions</Card>
+      </Box>
+
+    </Box>
+   
+    </div>
   )
 }
