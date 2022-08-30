932f51e6d078e1822cb4808b293fbfa5e4eef24b
[Evan McNeely] <evanmcneely@me.com>
2022-08-29 18:26:49 -0700

    removed comment

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
 ReactDOM.render(
   <React.StrictMode>
     <App />
