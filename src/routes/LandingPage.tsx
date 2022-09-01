feb4dbdd15e75a76b718688531a71dccf46a8764
[Evan McNeely] <evanmcneely@me.com>
2022-08-31 17:54:06 -0700

    got test environment working

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -7,7 +7,7 @@ import {Header} from '../components/Header';
 import {useNavigate} from 'react-router-dom';
 
 interface LandingPageProps { 
-    setUserName: React.Dispatch<React.SetStateAction<string>>;
+    setUserName: any;
     userName: string;
     socket: any;
 };
